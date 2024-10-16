16a3e6a9bcb990bdc937efb9baa89882398b7dcc
dibek <dibekpoudyal@gmail.com>
2024-10-16 13:47:48 +0545

    variables

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-5e004effba8578e1.d
@@ -0,0 +1,5 @@
+/mnt/d/rust-doc/projects/hello_cargo/target/debug/deps/libhello_cargo-5e004effba8578e1.rmeta: src/main.rs
+
+/mnt/d/rust-doc/projects/hello_cargo/target/debug/deps/hello_cargo-5e004effba8578e1.d: src/main.rs
+
+src/main.rs:
